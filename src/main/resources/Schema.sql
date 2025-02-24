db1ee967725478a5bacfee8ea322d6ea81bd4db8
sanjayhare <sanjayghare9595@gmail.com>
2025-02-24 09:06:47 +0530

    db

--- src/main/resources/Schema.sql
+++ src/main/resources/Schema.sql
@@ -70,3 +70,18 @@ CREATE TABLE order_items (
 	FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
 
+CREATE TABLE IF NOT EXISTS `address` (
+  `address_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `address1` varchar(200) NOT NULL,
+  `address2` varchar(200) DEFAULT NULL,
+  `city` varchar(50) NOT NULL,
+  `state` varchar(50) NOT NULL,
+  `zip_code` int NOT NULL,
+  `created_at` TIMESTAMP NOT NULL,
+  `created_by` varchar(50) NOT NULL,
+  `updated_at` TIMESTAMP DEFAULT NULL,
+  `updated_by` varchar(50) DEFAULT NULL,
+   FOREIGN KEY (user_id) REFERENCES users(person_Id),
+   PRIMARY KEY (`address_id`)
+);
